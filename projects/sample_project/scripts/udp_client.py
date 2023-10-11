c1e6b6878ccc7566551937b5bb4adab97c3acd8f
Sabar Dasgupta <s@bardasgupta.com>
2023-10-11 13:33:37 -0700

    continue debugging lights

--- projects/sample_project/scripts/udp_client.py
+++ projects/sample_project/scripts/udp_client.py
@@ -14,7 +14,7 @@ light_on_seq = {
   "action_seq": [
     {
       "action": "up",     # up or down
-      "duration": 50,    # in ms, -1 for keeping state up/down
+      "duration": 1000,    # in ms, -1 for keeping state up/down
       "delay_pre": -1,    # in ms, -1 for no delay before pulse
       "delay_post": -1,   # in ms, -1 for no delay after pulse
       "repeat": -1         # -1 for infinite
